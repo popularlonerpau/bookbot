0fb81239103b6585b0b6c14db286e33214e34651
popularlonerpau <phyeeaea@gmail.com>
2025-12-07 23:11:32 +0100

    added stats.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("greetings boots")
+def get_book_text(book):
+    with open(book) as f:
+        return f.read()
+
+
+def num_of_words(text):
+    dum = text.split()
+    return dum
+
+
+
+def main():
+    book = "books/frankenstein.txt"
+    text = get_book_text(book)
+    num = num_of_words(text)
+    print(text)
+    print(f'Found {len(num)} total words')
+    
+
+    
+
+
+main()
+
+    
